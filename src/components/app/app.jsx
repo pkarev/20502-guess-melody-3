27c6ac60e5d2509606dcf9589824bd75ab30990a
pavelkarev <pavel.karev@pegast.ru>
2020-02-13 18:03:31 +0300

    Add props types validation

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
+import PropTypes from 'prop-types';
 
-// eslint-disable-next-line react/prop-types
 const App = ({errorsCount}) => <WelcomeScreen errorsCount={errorsCount}/>;
+
+App.propTypes = {
+  errorsCount: PropTypes.number.isRequired,
+};
 
 export default App;
