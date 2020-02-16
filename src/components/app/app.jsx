23ded7f45bd1b656cd6231b91859e600a3e1094c
pavelkarev <pavel.karev@pegast.ru>
2020-02-16 23:57:25 +0300

    Configure react router and creade demo routes and game screens drafts for dev

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, {PureComponent} from "react";
 import PropTypes from 'prop-types';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
 import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
+import QuestionArtistScreen from '../question-artist-screen/question-artist-screen.jsx';
+import QuestionGenreScreen from '../question-genre-screen/question-genre-screen.jsx';
 
 const welcomeButtonHandler = () => {};
 
-const App = ({errorsCount}) => (
-  <WelcomeScreen
-    errorsCount={errorsCount}
-    onWelcomeButtonClick={welcomeButtonHandler}
-  />
-);
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const {errorsCount} = this.props;
+
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <WelcomeScreen
+              errorsCount={errorsCount}
+              onWelcomeButtonClick={welcomeButtonHandler}
+            />
+          </Route>
+          <Route exact path="/artist">
+            <QuestionArtistScreen/>
+          </Route>
+          <Route exact path="/genre">
+            <QuestionGenreScreen/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
 
 App.propTypes = {
   errorsCount: PropTypes.number.isRequired,
