7c4d93864f8a61445438065967923f5fa85c7ccb
pavelkarev <pavel.karev@pegast.ru>
2020-02-29 17:16:16 +0300

    Add Mistakes component

---
+++ src/components/miskates/mistakes.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Mistakes = ({count}) => (
+  <div className="game__mistakes">
+    {count &&
+      new Array(count).fill(``).map((item, index) => (
+        <div className="wrong" key={`mistake-${index}`}></div>
+      ))
+    }
+  </div>
+);
+
+Mistakes.propTypes = {
+  count: PropTypes.number.isRequired,
+};
+
+export default Mistakes;
